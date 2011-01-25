ab0f45f7e624bf688feaff2a258bba3aa8228a49
Brane F. Gračnar <bfg@frost.ath.cx>
2011-01-25 17:33:03 +0100

    Package si.planet9.mysql renamed to si.planet9.mysql.log.

--- src/main/java/si/planet9/mysql/log/Log4j.java
+++ src/main/java/si/planet9/mysql/log/Log4j.java
@@ -1,30 +1,28 @@
-package si.planet9.mysql;
+package si.planet9.mysql.log;
 
 import com.mysql.jdbc.log.Log;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Slf4j implements Log {
-
-	private static final Logger log = LoggerFactory.getLogger(Slf4j.class);
+public class Log4j implements Log {
+	private static final Logger log = Logger.getLogger(Log4j.class);
 	
-	public Slf4j(String name) {}
+	public Log4j (String name) {}
 
 	public boolean isDebugEnabled() {
 		return log.isDebugEnabled();
 	}
 
 	public boolean isErrorEnabled() {
-		return log.isErrorEnabled();
+		return log.isEnabledFor(Level.ERROR);
 	}
 
 	public boolean isFatalEnabled() {
-		return log.isErrorEnabled();
+		return log.isEnabledFor(Level.FATAL);
 	}
 
 	public boolean isInfoEnabled() {
-		return log.isInfoEnabled();
+		return log.isEnabledFor(Level.INFO);
 	}
 
 	public boolean isTraceEnabled() {
@@ -32,54 +30,54 @@ public class Slf4j implements Log {
 	}
 
 	public boolean isWarnEnabled() {
-		return log.isWarnEnabled();
+		return log.isEnabledFor(Level.WARN);
 	}
 
 	public void logDebug(Object msg) {
-		log.debug(msg.toString());
+		log.debug(msg);
 	}
 
 	public void logDebug(Object msg, Throwable e) {
-		log.debug(msg.toString(), e);
+		log.debug(msg, e);
 	}
 
 	public void logError(Object msg) {
-		log.error(msg.toString());
+		log.error(msg);
 	}
 
 	public void logError(Object msg, Throwable e) {
-		log.error(msg.toString(), e);
+		log.error(msg, e);
 	}
 
 	public void logFatal(Object msg) {
-		log.error(msg.toString());		
+		log.fatal(msg);		
 	}
 
 	public void logFatal(Object msg, Throwable e) {
-		log.error(msg.toString(), e);
+		log.fatal(msg, e);
 	}
 
 	public void logInfo(Object msg) {
-		log.info(msg.toString());
+		log.info(msg);
 	}
 
 	public void logInfo(Object msg, Throwable e) {
-		log.info(msg.toString(), e);
+		log.info(msg, e);
 	}
 
 	public void logTrace(Object msg) {
-		log.trace(msg.toString());
+		log.trace(msg);
 	}
 
 	public void logTrace(Object msg, Throwable e) {
-		log.trace(msg.toString(), e);
+		log.trace(msg, e);
 	}
 
 	public void logWarn(Object msg) {
-		log.warn(msg.toString());
+		log.warn(msg);
 	}
 
 	public void logWarn(Object msg, Throwable e) {
-		log.warn(msg.toString(), e);
+		log.warn(msg, e);
 	}
 }
--- src/main/java/si/planet9/mysql/log/Slf4j.java
+++ src/main/java/si/planet9/mysql/log/Slf4j.java
@@ -1,28 +1,30 @@
-package si.planet9.mysql;
+package si.planet9.mysql.log;
 
 import com.mysql.jdbc.log.Log;
-import org.apache.log4j.Logger;
-import org.apache.log4j.Level;
 
-public class Log4j implements Log {
-	private static final Logger log = Logger.getLogger(Log4j.class);
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4j implements Log {
+
+	private static final Logger log = LoggerFactory.getLogger(Slf4j.class);
 	
-	public Log4j (String name) {}
+	public Slf4j(String name) {}
 
 	public boolean isDebugEnabled() {
 		return log.isDebugEnabled();
 	}
 
 	public boolean isErrorEnabled() {
-		return log.isEnabledFor(Level.ERROR);
+		return log.isErrorEnabled();
 	}
 
 	public boolean isFatalEnabled() {
-		return log.isEnabledFor(Level.FATAL);
+		return log.isErrorEnabled();
 	}
 
 	public boolean isInfoEnabled() {
-		return log.isEnabledFor(Level.INFO);
+		return log.isInfoEnabled();
 	}
 
 	public boolean isTraceEnabled() {
@@ -30,54 +32,54 @@ public class Log4j implements Log {
 	}
 
 	public boolean isWarnEnabled() {
-		return log.isEnabledFor(Level.WARN);
+		return log.isWarnEnabled();
 	}
 
 	public void logDebug(Object msg) {
-		log.debug(msg);
+		log.debug(msg.toString());
 	}
 
 	public void logDebug(Object msg, Throwable e) {
-		log.debug(msg, e);
+		log.debug(msg.toString(), e);
 	}
 
 	public void logError(Object msg) {
-		log.error(msg);
+		log.error(msg.toString());
 	}
 
 	public void logError(Object msg, Throwable e) {
-		log.error(msg, e);
+		log.error(msg.toString(), e);
 	}
 
 	public void logFatal(Object msg) {
-		log.fatal(msg);		
+		log.error(msg.toString());		
 	}
 
 	public void logFatal(Object msg, Throwable e) {
-		log.fatal(msg, e);
+		log.error(msg.toString(), e);
 	}
 
 	public void logInfo(Object msg) {
-		log.info(msg);
+		log.info(msg.toString());
 	}
 
 	public void logInfo(Object msg, Throwable e) {
-		log.info(msg, e);
+		log.info(msg.toString(), e);
 	}
 
 	public void logTrace(Object msg) {
-		log.trace(msg);
+		log.trace(msg.toString());
 	}
 
 	public void logTrace(Object msg, Throwable e) {
-		log.trace(msg, e);
+		log.trace(msg.toString(), e);
 	}
 
 	public void logWarn(Object msg) {
-		log.warn(msg);
+		log.warn(msg.toString());
 	}
 
 	public void logWarn(Object msg, Throwable e) {
-		log.warn(msg, e);
+		log.warn(msg.toString(), e);
 	}
 }
